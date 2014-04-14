f54186a729ac5788edb6dcf14763fa1fd425c486
judening <jning@ngpvan.com>
2014-04-13 22:15:16 -0400

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -1,5 +1,3 @@
-import random
-
 def quick_sort(teh_list,left,right):
     if (right-left) <=0:
          return
@@ -9,18 +7,17 @@ def quick_sort(teh_list,left,right):
         pivot = start
         while start < end:
             
-            while(teh_list[start] <= teh_list[pivot]):
+            while(teh_list[start] <= teh_list[pivot]) and start<end:
                 start+=1
-            while(teh_list[end] > teh_list[pivot]):
+            while(teh_list[end] > teh_list[pivot]) and start<=end:
                 end-=1
                 
             if start < end:
                 teh_list[start],teh_list[end] = teh_list[end] , teh_list[start]
-                
-        quick_sort(teh_list,left,pivot-1)
-        quick_sort(teh_list,pivot+1,right)
+        teh_list[pivot],teh_list[end] = teh_list[end], teh_list[pivot]        
+        quick_sort(teh_list,left,end-1)
+        quick_sort(teh_list,end+1,right)
 
 def quicksort(teh_list):
     quick_sort(teh_list,0,len(teh_list)-1)
-    print teh_list    
-        
+    print teh_list  
